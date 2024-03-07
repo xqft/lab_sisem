4bcb4764bc2842a4181ea235f463022f97fc6abd
xqft <estefano.bargas@fing.edu.uy>
2024-03-07 09:46:44 -0300

    Commit inicial

---
+++ test/lab1/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ccstudio/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /opt/ccstudio/ccs/ccs_base/msp430/include/msp430g2533.h
+main.obj: /opt/ccstudio/ccs/ccs_base/msp430/include/in430.h
+main.obj: /opt/ccstudio/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /opt/ccstudio/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c:
+
+/opt/ccstudio/ccs/ccs_base/msp430/include/msp430.h:
+
+/opt/ccstudio/ccs/ccs_base/msp430/include/msp430g2533.h:
+
+/opt/ccstudio/ccs/ccs_base/msp430/include/in430.h:
+
+/opt/ccstudio/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/opt/ccstudio/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
