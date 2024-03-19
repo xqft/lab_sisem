3d2897427c97c69cb4dbc01badfb961d9d326266
Juan Silveira <juanandressilveira13@gmail.com>
2024-03-19 11:09:00 -0300

    parte1sa

--- lab2-parte1/main.c
+++ lab2-parte1/main.c
@@ -1,28 +1,12 @@
 #include <msp430.h> 
-#define LED1 (0x0001)
-#include "timer_hw.h"
+
+
 /**
  * main.c
  */
-
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-    int ESTADO_LED = 0;
-	P1DIR |= LED1; // Configura pin LED1 salida
-	_enable_interrupt();
-	while(1){
-	    P1OUT = ESTADO_LED;
-	}
-	return 0;
 	
-}
-
-
-
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void int_timer_A (void){
-
-    ESTADO_LED ^= LED1; // Conmuta LED1 usando XOR
-
+	return 0;
 }
---
+++ test/lab2-parte1/main.c
@@ -0,0 +1,26 @@
+#include <msp430.h> 
+#define LED1 (0x0001)
+#include "timer_hw.h"
+/**
+ * main.c
+ */
+    int ESTADO_LED = 0;
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1DIR |= LED1; // Configura pin LED1 salida
+    _enable_interrupt();
+    while(1){
+        P1OUT = ESTADO_LED;
+    }
+
+}
+
+
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void int_timer_A (void){
+
+    ESTADO_LED ^= LED1; // Conmuta LED1 usando XOR
+
+}
