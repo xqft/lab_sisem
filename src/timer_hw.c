3e872038425d2f8d582b829ac7c722a5acd01065
Juan Silveira <juanandressilveira13@gmail.com>
2024-03-19 09:47:05 -0300

    inicializacion timer

--- src/timer_hw.c
+++ src/timer_hw.c
@@ -3,16 +3,22 @@
 
 void config_timer_crystal()
 {
-    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A
-    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador LFXT1 (selecciona low frequency)
+    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A.
+    TACTL |= MC_1           // Modo 1 para el Timer A: cuenta de forma ascendente.
+    TACCR0 = 8192           // Constante a decrementar por el Timer A cada t=1/32768.
+    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador LFXT1 (selecciona low frequency).
     BCSCTL1 |= DIVA_0;      // Divisor en /1.
-    BCSCTL3 |= LFXT1S_0;    // Cristal de 32768-Hz para el oscilador LFXT1
+    BCSCTL3 |= LFXT1S_0;    // Cristal de 32768-Hz para el oscilador LFXT1.
+    TACTL |= TAIE           // Habilito las interrupciones del Timer A.
 }
 
 void config_timer_VLO(){
 
-    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A
-    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador VLO (selecciona low frequency)
+    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A.
+    TACTL |= MC_1           // Modo 1 para el Timer A: cuenta de forma ascendente.
+    TACCR0 = 3000           // Constante a decrementar por el Timer A cada t=1/12000.
+    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador VLO (selecciona low frequency).
     BCSCTL1 |= DIVA_0;      // Divisor en /1.
-    BCSCTL3 |= LFXT1S_2;    // Oscilador VLO de frecuencia 12-KHz
+    BCSCTL3 |= LFXT1S_2;    // Oscilador VLO de frecuencia 12-KHz.
+    TACTL |= TAIE           // Habilito las interrupciones del Timer A.
 }
