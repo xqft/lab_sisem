7f2c44c72813070798bfcdf16c4f2e223efbafca
xqft <estefano.bargas@fing.edu.uy>
2024-03-19 22:57:22 +0000

    Comment out flag

--- src/timer_hw.c
+++ src/timer_hw.c
@@ -25,6 +25,6 @@ void config_timer_VLO(){
     BCSCTL1 |= DIVA_0;          // Divisor en /1.
     BCSCTL3 |= LFXT1S_2;        // Oscilador VLO de frecuencia 12-KHz.
 
-    TACTL   |= TAIE;             // Habilito las interrupciones del Timer A.
+    //TACTL   |= TAIE;             // Habilito las interrupciones del Timer A.
     TACCTL0 |= CCIE;            // Habilito las interrupciones del Timer A en modo comparacion.
 }
