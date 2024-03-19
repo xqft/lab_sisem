b89c138811483b5b261728487515f99e8437c322
Lucas <lucaspereyrarv@gmail.com>
2024-03-18 22:46:57 -0300

    rutina atencion VLO

--- include/timer_hw.h
+++ include/timer_hw.h
@@ -2,3 +2,5 @@
 
 /// Rutina de inicializacion del timer hardware usando el cristal como fuente de reloj.
 void config_timer_crystal();
+/// Rutina de inicializacion del timer hardware usando el VLO como fuente de reloj.
+void config_timer_VLO();
--- src/timer_hw.c
+++ src/timer_hw.c
@@ -8,3 +8,11 @@ void config_timer_crystal()
     BCSCTL1 |= DIVA_0;      // Divisor en /1.
     BCSCTL3 |= LFXT1S_0;    // Cristal de 32768-Hz para el oscilador LFXT1
 }
+
+void config_timer_VLO(){
+
+    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A
+    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador VLO (selecciona low frequency)
+    BCSCTL1 |= DIVA_0;      // Divisor en /1.
+    BCSCTL3 |= LFXT1S_2;    // Oscilador VLO de frecuencia 12-KHz
+}
