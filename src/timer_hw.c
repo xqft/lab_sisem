31365461be0c0854a2d0203bf88a3580f129bf8d
xqft <estefano.bargas@fing.edu.uy>
2024-03-19 01:33:31 +0000

    Finish config

--- src/timer_hw.c
+++ src/timer_hw.c
@@ -3,6 +3,8 @@
 
 void config_timer_crystal()
 {
-    TACTL |= TASSEL_1; // Selecciono ACLK para el Timer A
-    BCSCTL1 |= LFXT1S0 | DIVA_0; // Modo 0 (low frec) para LFXT1 (crystal), divisor en 1
+    TACTL |= TASSEL_1;      // Selecciono ACLK para el Timer A
+    BCSCTL1 &= ~XTS;        // Modo 0 para el oscilador LFXT1 (selecciona low frequency)
+    BCSCTL1 |= DIVA_0;      // Divisor en /1.
+    BCSCTL3 |= LFXT1S_0;    // Cristal de 32768-Hz para el oscilador LFXT1
 }
