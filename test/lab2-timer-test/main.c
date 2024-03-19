80eaa5ce81ddd9fc73c2c7664c0cf05a2d3da24d
xqft <estefano.bargas@fing.edu.uy>
2024-03-19 23:45:23 +0000

    Added inc_time() in ISR

--- test/lab2-timer-test/main.c
+++ test/lab2-timer-test/main.c
@@ -1,12 +1,25 @@
 #include <msp430.h> 
+#include <stdio.h>
+#include "timer_hw.h"
+#include "timer.h"
 
+#define LED1 (0x0001)
 
-/**
- * main.c
- */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1DIR |= LED1; // Configura pin LED1 salida
+    config_timer_crystal();
+    //config_timer_VLO();
+    _enable_interrupt();
+
+    for(;;) {}
+    return 0;
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void int_timer_A (void)
+{
+    P1OUT ^= LED1;  // Conmuta LED1 usando XOR
+    inc_time();     // Incrementa 250 ms
 }
