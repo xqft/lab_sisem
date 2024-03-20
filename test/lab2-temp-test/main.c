81320f76117891336925b3e9330ec82919dd4bb6
Juan Silveira <juanandressilveira13@gmail.com>
2024-03-20 20:25:33 -0300

    parte4avance

--- test/lab2-temp-test/main.c
+++ test/lab2-temp-test/main.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    volatile int32_t temperatura;        //Variable que toma el valor de la temperatura
+    volatile char* flag_de_main;         //Puntero a flag
+    *flag_de_main = 0;          //Arranco con flag baja
+    setFlagTemp(flag_de_main);  //Apunto flag a flag_de_main
 	initTemp();
 	runTemp();
-	int32_t temperatura;        //Variable que toma el valor de la temperatura
-	char* flag_de_main;         //Puntero a flag
-	*flag_de_main = 0;          //Arranco con flag baja
-	setFlagTemp(flag_de_main);  //Apunto flag a flag_de_main
+
 	
 	_enable_interrupt();        //Habilito interrupciones
 
