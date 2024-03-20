e093f17771002bc4aa8c7ef63f703a93b943619b
Juan Silveira <juanandressilveira13@gmail.com>
2024-03-20 20:15:24 -0300

    parte4

--- test/lab2-temp-test/main.c
+++ test/lab2-temp-test/main.c
@@ -1,12 +1,25 @@
 #include <msp430.h> 
-
+#include "temperatura.h"
 
 /**
  * main.c
  */
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	initTemp();
+	runTemp();
+	int32_t temperatura;        //Variable que toma el valor de la temperatura
+	char* flag_de_main;         //Puntero a flag
+	*flag_de_main = 0;          //Arranco con flag baja
+	setFlagTemp(flag_de_main);  //Apunto flag a flag_de_main
 	
-	return 0;
+	_enable_interrupt();        //Habilito interrupciones
+
+	for(;;){
+	    if (*flag_de_main == 1){
+	        temperatura = getTemp();
+	    }
+	}
 }
