3ee61e90e80eec1a13ebbb8286c1633f1aab1daf
Juan Silveira <juanandressilveira13@gmail.com>
2024-04-02 11:28:05 -0300

    avance part1

--- src/uart.c
+++ src/uart.c
@@ -6,6 +6,15 @@
  */
 #include "timer.h"
 
+
+static uint8_t * Tx_data;
+static uint8_t Tx_largo;
+static char Rx_data[10];
+static uint8_t cont_pos;
+static volatile uint8_t Rx_largo;
+
+static volatile char*    flag_Rx;
+
 void initP1 (){
     P1SEL |= BIT1 + BIT2;       //Set pines RXD y TXD
     P1SEL2 |= BIT1 + BIT2;      // ""
@@ -28,6 +37,55 @@ void initUART(){
     UCA0MCTL = 3<<1;            //""
 
 
-
 }
 
+void Tx_int (uint8_t * data_ptr, uint8_t largo){
+    Tx_data = data_ptr;
+    Tx_largo = largo;
+    UCA0TXBUF = * Tx_data;
+    IE2 |= UCA0TXIE; // Habilito interrupciones de registro vacio
+    cont_pos = 1;
+}
+
+#pragma vector = USCIAB0TX_VECTOR
+__interrupt void Tx_isr(void){
+    cont_pos++;
+    if cont_pos <= Tx_largo{
+        Tx_data++;
+        UCA0TXBUF = * Tx_data;
+    }
+    else{
+    IE2 &= !UCA0TXIE;
+    cont_pos = 0;
+    }
+}
+
+#pragma vector = USCIAB0RX_VECTOR
+__interrupt void Rx_isr(void){
+
+    char received_char = UCA0RXBUF; // Leer el carácter recibido
+
+    if (received_char == '\r') {
+                  // rx_message_complete = 1; // Activar la bandera de mensaje completo
+                  * flag_Rx = 1;
+    }
+
+        // Almacenar el carácter en el buffer de recepción
+    else if (Rx_largo < 10) {
+           Rx_data[Rx_largo++] = received_char;
+           // Verificar si se ha recibido el carácter de fin de mensaje
+    }
+}
+
+void copy_Rx_buff(char *Rx_buff){
+    int i;
+    for(i = 0; i < Rx_largo; i++){
+    Rx_buff[i] = Rx_data[i];
+    }
+    Rx_largo = 0;
+}
+
+void setFlagRx(char* flag_main){
+    /*Set flag*/
+    flag_Rx = flag_main;        //En main inicializar flag_main en cero
+}
