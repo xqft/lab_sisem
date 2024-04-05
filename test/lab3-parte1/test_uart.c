b8943fc68135edfe5b5280e58a6fb524d5e458bf
lucas <lucaspereyra@MacBook-de-Lucas.local>
2024-04-05 09:13:56 -0300

    jtest_uart act

--- test/lab3-parte1/test_uart.c
+++ test/lab3-parte1/test_uart.c
@@ -7,7 +7,8 @@
  */
 
 static volatile char msg[4] = "hola";
-static volatile uint8_t flag_rx_main = 0;
+static volatile char* msg[10];
+static volatile uint8_t * flag_rx_main = 0;
 
 int main(void)
 {
@@ -16,12 +17,25 @@ int main(void)
 	p1_init();
 	uart_init();
     uart_transmit(&msg, 4);
+    set_flag_rx(&flag_rx_main);
+
     _enable_interrupt();
-	while (1) {
-	   // UCA0TXBUF = 'A';
-	    int i;
-	    for (i = 0; i < 10000; i++) {}
-	}
+while(1){
+
+    if(*flag_rx_main == 1){
+
+            *flag_rx_main =0;
+
+            copy_rx_buff(&msgg);
+            uart_transmit(&msgg,1);
+
+            while (1) {
+                    int i;
+                    for (i = 0; i < 10000; i++) {}
+            }
+    }
+
+}
 
 	return 0;
 }
