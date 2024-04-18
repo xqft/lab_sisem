bf388281b7b7288600786ca69c7aebd2c575569b
xqft <estefano.bargas@fing.edu.uy>
2024-04-18 20:43:03 -0300

    Fix rx

--- include/uart.h
+++ include/uart.h
@@ -15,6 +15,7 @@ void uart_init();
 void uart_transmit(uint8_t *data, uint8_t length);
 void copy_rx_buff(char *external_buff, uint8_t *length);
 void set_flag_rx(uint8_t *flag);
+void set_flag_error_rx(uint8_t *flag);
 
 
 #endif /* INCLUDE_UART_H_ */
