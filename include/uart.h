b5c120899e3dd914ed3f83376ac86582bd4e7da7
xqft <estefano.bargas@fing.edu.uy>
2024-04-18 21:03:05 -0300

    Add docs

--- include/uart.h
+++ include/uart.h
@@ -10,11 +10,15 @@
 
 /// Inicializar pines de transmision UART del puerto 1.
 void p1_init();
-/// Inicializar configuracion
+/// Inicializar configuracion del modulo UART
 void uart_init();
+/// Transmitir un mensaje por UART
 void uart_transmit(uint8_t *data, uint8_t length);
+/// Copiar un mensaje recibido por UART
 void copy_rx_buff(char *external_buff, uint8_t *length);
+/// Establecer la flag de recepcion
 void set_flag_rx(uint8_t *flag);
+/// Establecer la flag de error de recepcion
 void set_flag_error_rx(uint8_t *flag);
 
 
