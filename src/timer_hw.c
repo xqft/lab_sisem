ac4c55df34f53a9f6cf5c5632833fc3e10076655
Estefano Lautaro Bargas Nuñez <estefano.bargas@fing.edu.uy>
2024-04-18 21:53:15 -0300

    Lab4 parte1

--- src/timer_hw.c
+++ src/timer_hw.c
@@ -8,32 +8,30 @@ volatile static uint32_t counter = 0;
 static uint8_t *counter_flag;
 static uint32_t counter_max = 0;
 
-void config_timer_crystal()
-{
-    TACTL   |= TASSEL_1;        // Selecciono ACLK para el Timer A.
-    TACTL   |= MC_1;            // Modo 1 para el Timer A: cuenta de forma ascendente.
-    TACCR0  = 8192;             // Constante a decrementar por el Timer A cada t=1/32768.
+void config_timer_crystal() {
+	TACTL |= TASSEL_1;        // Selecciono ACLK para el Timer A.
+	TACTL |= MC_1;        // Modo 1 para el Timer A: cuenta de forma ascendente.
+	TACCR0 = 8192;     // Constante a decrementar por el Timer A cada t=1/32768.
 
+	BCSCTL1 &= ~XTS; // Modo 0 para el oscilador LFXT1 (selecciona low frequency).
+	BCSCTL1 |= DIVA_0;          // Divisor en /1.
+	BCSCTL3 |= LFXT1S_0;        // Cristal de 32768-Hz para el oscilador LFXT1.
 
-    BCSCTL1 &= ~XTS;            // Modo 0 para el oscilador LFXT1 (selecciona low frequency).
-    BCSCTL1 |= DIVA_0;          // Divisor en /1.
-    BCSCTL3 |= LFXT1S_0;        // Cristal de 32768-Hz para el oscilador LFXT1.
-
-    //TACTL   |= TAIE;            // Habilito las interrupciones del Timer A.
-    TACCTL0 |= CCIE;            // Habilito las interrupciones del Timer A en modo comparacion.
+	//TACTL   |= TAIE;            // Habilito las interrupciones del Timer A.
+	TACCTL0 |= CCIE; // Habilito las interrupciones del Timer A en modo comparacion.
 }
 
-void config_timer_VLO(){
-    TACTL   |= TASSEL_1;        // Selecciono ACLK para el Timer A.
-    TACTL   |= MC_1;            // Modo 1 para el Timer A: cuenta de forma ascendente.
-    TACCR0  = 3000;             // Constante a decrementar por el Timer A cada t=1/12000.
+void config_timer_VLO() {
+	TACTL |= TASSEL_1;        // Selecciono ACLK para el Timer A.
+	TACTL |= MC_1;        // Modo 1 para el Timer A: cuenta de forma ascendente.
+	TACCR0 = 3000;     // Constante a decrementar por el Timer A cada t=1/12000.
 
-    BCSCTL1 &= ~XTS;            // Modo 0 para el oscilador VLO (selecciona low frequency).
-    BCSCTL1 |= DIVA_0;          // Divisor en /1.
-    BCSCTL3 |= LFXT1S_2;        // Oscilador VLO de frecuencia 12-KHz.
+	BCSCTL1 &= ~XTS; // Modo 0 para el oscilador VLO (selecciona low frequency).
+	BCSCTL1 |= DIVA_0;          // Divisor en /1.
+	BCSCTL3 |= LFXT1S_2;        // Oscilador VLO de frecuencia 12-KHz.
 
-    //TACTL   |= TAIE;             // Habilito las interrupciones del Timer A.
-    TACCTL0 |= CCIE;            // Habilito las interrupciones del Timer A en modo comparacion.
+	//TACTL   |= TAIE;             // Habilito las interrupciones del Timer A.
+	TACCTL0 |= CCIE; // Habilito las interrupciones del Timer A en modo comparacion.
 }
 
 void set_counter_flag(uint8_t *flag) {
@@ -45,14 +43,14 @@ void set_counter_max(uint32_t max) {
 }
 
 #pragma vector = TIMER0_A0_VECTOR
-__interrupt void int_timer_A (void){
+__interrupt void int_timer_A(void) {
 	P1OUT ^= LED1;  // Conmuta LED1 usando XOR
 	inc_time();
 	if (counter >= counter_max) {
 		*counter_flag = 1;
 		counter = 0;
-	}else{
-	    counter++;
+		__low_power_mode_off_on_exit();
+	} else {
+		counter++;
 	}
-
 }
--- src/uart.c
+++ src/uart.c
@@ -33,41 +33,38 @@ static volatile uint8_t *rx_error_flag;
 /// Bandera que bloquea la recepcion
 static volatile uint8_t rx_block_flag;
 
-void p1_init()
-{
-    P1SEL |= BIT1 + BIT2;       // Set pines RXD y TXD
-    P1SEL2 |= BIT1 + BIT2;      // ""
+void p1_init() {
+	P1SEL |= BIT1 + BIT2;       // Set pines RXD y TXD
+	P1SEL2 |= BIT1 + BIT2;      // ""
 }
 
-void uart_init()
-{
-    UCA0CTL1 |= UCSWRST;    // Set reset
+void uart_init() {
+	UCA0CTL1 |= UCSWRST;    // Set reset
 
-    // Estado por defecto luego de un reset
-    // UCA0CTL0 &= ~UCPEN;  // Set paridad off
-    // UCA0CTL0 |= UC7BIT;  // Set 8-bit
+	// Estado por defecto luego de un reset
+	// UCA0CTL0 &= ~UCPEN;  // Set paridad off
+	// UCA0CTL0 |= UC7BIT;  // Set 8-bit
 
-    UCA0CTL1 |= UCSSEL_1;   // Set fuente ACLK
-    UCA0CTL1 |= UCSSEL0;    // ""
+	UCA0CTL1 |= UCSSEL_1;   // Set fuente ACLK
+	UCA0CTL1 |= UCSSEL0;    // ""
 
-    // Estado por defecto luego de un reset
-    // BCSCTL1 &= ~XTS;     // Modo 0 para el oscilador LFXT1 (selecciona low frequency).
-    // BCSCTL1 |= DIVA_0;   // Divisor en /1.
-    // BCSCTL3 |= LFXT1S_0; // Cristal de 32768-Hz para el oscilador LFXT1.
+	// Estado por defecto luego de un reset
+	// BCSCTL1 &= ~XTS;     // Modo 0 para el oscilador LFXT1 (selecciona low frequency).
+	// BCSCTL1 |= DIVA_0;   // Divisor en /1.
+	// BCSCTL3 |= LFXT1S_0; // Cristal de 32768-Hz para el oscilador LFXT1.
 
-    // Establecer baud-rate 9600 con fuente de 32 kHz.
-    // Ref: Tabla 15-4, Family User's Guide.
-    UCA0BR1 = 0x00;
-    UCA0BR0 = 0x03;
-    UCA0MCTL |= UCBRS_3;
-    UCA0MCTL |= UCBRF_0;
+	// Establecer baud-rate 9600 con fuente de 32 kHz.
+	// Ref: Tabla 15-4, Family User's Guide.
+	UCA0BR1 = 0x00;
+	UCA0BR0 = 0x03;
+	UCA0MCTL |= UCBRS_3;
+	UCA0MCTL |= UCBRF_0;
 
-    UCA0CTL1 &= ~UCSWRST;   // Reestablecer reset
-    IE2 |= UCA0RXIE;        // Habilito interrupciones de recepcion
+	UCA0CTL1 &= ~UCSWRST;   // Reestablecer reset
+	IE2 |= UCA0RXIE;        // Habilito interrupciones de recepcion
 }
 
-void uart_transmit(uint8_t *data, uint8_t length)
-{
+void uart_transmit(uint8_t *data, uint8_t length) {
 	const uint8_t *error_msg = "tx overflow";
 
 	if (length > TX_DATA_MAX_LEN) {
@@ -75,83 +72,75 @@ void uart_transmit(uint8_t *data, uint8_t length)
 		length = strlen(data);
 	}
 
-    memcpy(tx_data, data, length);
-    tx_data_length = length;
+	memcpy(tx_data, data, length);
+	tx_data_length = length;
 
-    // Transmito el primer byte
-    UCA0TXBUF = tx_data[0];
-    tx_data_count = 1;
+	// Transmito el primer byte
+	UCA0TXBUF = tx_data[0];
+	tx_data_count = 1;
 
-    // Habilito interrupcion de registro vacio
-    IE2 |= UCA0TXIE;
+	// Habilito interrupcion de registro vacio
+	IE2 |= UCA0TXIE;
 }
 
-void copy_rx_buff(char *external_buff, uint8_t *length)
-{
-    memcpy(external_buff, rx_data, rx_data_length);
-    *length = rx_data_length;
+void copy_rx_buff(char *external_buff, uint8_t *length) {
+	memcpy(external_buff, rx_data, rx_data_length);
+	*length = rx_data_length;
 }
 
-void set_flag_rx(uint8_t *flag)
-{
-    rx_received_flag = flag;   // En main inicializar flag en cero
+void set_flag_rx(uint8_t *flag) {
+	rx_received_flag = flag;   // En main inicializar flag en cero
 }
 
-void set_flag_error_rx(uint8_t *flag)
-{
+void set_flag_error_rx(uint8_t *flag) {
 	rx_error_flag = flag;
 }
 
 #pragma vector = USCIAB0TX_VECTOR
-__interrupt void tx_isr(void)
-{
-    if (tx_data_count < tx_data_length)
-    {
-        UCA0TXBUF = tx_data[tx_data_count];
-        tx_data_count++;
-    }
-    else
-    {
-        IE2 &= ~UCA0TXIE;
-        tx_data_count = 0;
-    }
+__interrupt void tx_isr(void) {
+	if (tx_data_count < tx_data_length) {
+		UCA0TXBUF = tx_data[tx_data_count];
+		tx_data_count++;
+	} else {
+		IE2 &= ~UCA0TXIE;
+		tx_data_count = 0;
+	}
+	__low_power_mode_off_on_exit();
 }
 
 #pragma vector = USCIAB0RX_VECTOR
-__interrupt void rx_isr(void)
-{
-    // Siguiente dato disponible del buffer de recepcion
-    static volatile uint8_t rx_data_count = 0;
+__interrupt void rx_isr(void) {
+	// Siguiente dato disponible del buffer de recepcion
+	static volatile uint8_t rx_data_count = 0;
 
-    char received_char = UCA0RXBUF; // Leer el caracter recibido
+	char received_char = UCA0RXBUF; // Leer el caracter recibido
 
-    if (received_char == '\r')
-    {
-    	if (rx_block_flag) {
-    		// Al terminar el mensaje invalido, levanto el bloqueo
-    		rx_block_flag = 0;
-    		return;
-    	}
+	if (received_char == '\r') {
+		if (rx_block_flag) {
+			// Al terminar el mensaje invalido, levanto el bloqueo
+			rx_block_flag = 0;
+			return;
+		}
 
-        rx_data_length = rx_data_count;
-        rx_data_count = 0;
-        *rx_received_flag = 1;
-    }
-    else if (rx_data_count < RX_DATA_MAX_LEN) // saturar en el limite del buffer
-    {
-    	if (rx_block_flag) {
-    		// Si hay un error, bloqueo la recepcion hasta terminar el mensaje.
-    		return;
-    	}
+		rx_data_length = rx_data_count;
+		rx_data_count = 0;
+		*rx_received_flag = 1;
+	} else if (rx_data_count < RX_DATA_MAX_LEN) // saturar en el limite del buffer
+	{
+		if (rx_block_flag) {
+			// Si hay un error, bloqueo la recepcion hasta terminar el mensaje.
+			return;
+		}
 
-        rx_data[rx_data_count] = received_char;
-        rx_data_count++;
-    } else {
-    	// Buffer overflow, bloqueo la recepcion y reinicio el buffer
-    	rx_data_length = 0;
-    	rx_data_count = 0;
+		rx_data[rx_data_count] = received_char;
+		rx_data_count++;
+	} else {
+		// Buffer overflow, bloqueo la recepcion y reinicio el buffer
+		rx_data_length = 0;
+		rx_data_count = 0;
 
-    	*rx_error_flag = 1;
-    	rx_block_flag = 1;
-    }
+		*rx_error_flag = 1;
+		rx_block_flag = 1;
+	}
+	__low_power_mode_off_on_exit();
 }
