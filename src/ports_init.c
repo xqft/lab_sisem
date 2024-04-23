339c3b75850e13b51c6c910b5c7da6ceb0b68ae1
Lucas <lucaspereyrarv@gmail.com>
2024-04-23 10:33:50 -0300

    Parte4 prueba

---
+++ src/ports_init.c
@@ -0,0 +1,23 @@
+/*
+ * ports_init.c
+ *
+ *  Created on: Apr 23, 2024
+ *      Author: lucas
+ */
+#include <msp430.h>
+
+
+void p2_p3_init(){
+
+    //P1DIR |= BIT3;
+    //P2DIR |= BIT0 +BIT3 + BIT4 + BIT5 + BIT7;
+
+        P1DIR = 0xFF;
+        P1OUT = BIT3;
+
+        P2DIR = 0xFF;
+        P2OUT = 0x00;
+
+        P3DIR = 0xFF;
+        P3OUT = 0x00;
+}
