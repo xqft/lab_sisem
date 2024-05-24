f647ec8173e80c4552b7d77aaaa3fd3b8d20136a
xqft <estefano.bargas@fing.edu.uy>
2024-05-23 23:04:15 -0300

    add get neighbours

--- src/fuzzy.c
+++ src/fuzzy.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #include "fuzzy.h"
 
 void fuzzy_edge_detect(uint8_t* data) {
@@ -5,5 +7,37 @@ void fuzzy_edge_detect(uint8_t* data) {
 }
 
 uint8_t get_neighbours(uint8_t* data, uint16_t index) {
+	uint16_t result = 0;
 
+	// inicializo al primer vecino
+	uint8_t* neighbour = data[index - 22];
+	result |= *neighbour & 1;
+
+	// segundo vecino
+	neighbour += 1;
+	result |= (*neighbour & 1) << 1;
+
+	// tercer vecino
+	neighbour += 1;
+	result |= (*neighbour & 1) << 2;
+
+	// cuarto vecino
+	neighbour += 19;
+	result |= (*neighbour & 1) << 3;
+
+	// quinto vecino
+	neighbour += 2;
+	result |= (*neighbour & 1) << 4;
+
+	// sexto vecino
+	neighbour += 19;
+	result |= (*neighbour & 1) << 5;
+
+	// septimo vecino
+	neighbour += 1;
+	result |= (*neighbour & 1) << 6;
+
+	// octavo vecino
+	neighbour += 1;
+	result |= (*neighbour & 1) << 7;
 }
