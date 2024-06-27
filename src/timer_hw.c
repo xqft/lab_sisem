3615d4e8f98325e720edbc6fc4622dfa446bcfeb
Lucas <lucaspereyrarv@gmail.com>
2024-06-27 12:13:11 -0300

    Set frec module

--- src/timer_hw.c
+++ src/timer_hw.c
@@ -106,3 +106,19 @@ inline uint16_t get_timer_capture()
     float timer_exacto = TAR/16.339;
     return timer_exacto;
 }
+
+void CPU_16MHz()
+{
+    if (CALBC1_16MHZ != 0xFF) { // Verificar si los valores de calibración están presentes
+               DCOCTL = 0;             // Limpiar los bits de DCO
+               BCSCTL1 = CALBC1_16MHZ; // Establecer el rango de DCO a 16 MHz
+               DCOCTL = CALDCO_16MHZ;  // Establecer DCO a 16 MHz
+           } else {
+               // Manejar el caso en que los valores de calibración no estén presentes
+               // Esto podría significar que el microcontrolador no tiene valores de calibración cargados
+               while (1); // Bucle infinito, o cualquier otra medida de recuperación
+           }
+
+           // Configurar MCLK para usar el DCO a 16 MHz
+           BCSCTL2 = SELM_0 | DIVM_0 | DIVS_0; // MCLK = DCO, SMCLK = DCO, sin divisiones
+}
